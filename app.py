e43b83a76676336711a7425510042349f3ab2ed6
Jyew <jyew1992@gmail.com>
2022-01-05 12:06:18 +0800

    update port number

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ parser = reqparse.RequestParser()
 
 # configure to be environment variable later
 kafka_topic = 'tweets'
-bootstrap_servers = 'my-route-without-auth-amq-streams.apps.cluster-4987.4987.sandbox1668.opentlc.com:80'
+bootstrap_servers = 'my-route-without-auth-amq-streams.apps.cluster-4987.4987.sandbox1668.opentlc.com'
 
 
 @app.route('/')
