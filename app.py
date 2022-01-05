dd8ed98fe40d02183b0576771ff17ae4ed91ff8a
Jyew <jyew1992@gmail.com>
2022-01-05 14:37:09 +0800

    testing without flush

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ class send_data_to_kafka(Resource):
             # 'followers': status.user.followers_count
         }      
         producer.send(kafka_topic, data)
-        producer.flush()
+        #producer.flush()
         return 200
 
     # def post(self):
